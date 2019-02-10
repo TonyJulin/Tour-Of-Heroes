1b1bc964363338038e796af6e9ce8e1a8b0a49a4
TonyJulin <tonyjulin@gmail.com>
2019-02-10 16:20:48 -0700

    Added hero with two way binding. Displayed a list of heroes

---
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  hero: Hero = {
+    id: 1337,
+    name: 'SideWinder'
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Cena' },
+  { id: 12, name: 'Blastose' },
+  { id: 13, name: 'Phrophylactus' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magna' },
+  { id: 16, name: 'The Stretch' },
+  { id: 17, name: 'Duko' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Zuko Blyat' },
+  { id: 20, name: 'Tornado' }
+];
