8862e11c2b51d865f99d4ead697377f4361eb41c
TonyJulin <tonyjulin@gmail.com>
2019-02-11 00:04:59 -0700

    Starting to Angular services and dependency injection into app

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +8,28 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeroes();
+  }
 
+  // Functions
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  // getHeroes() with observable
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  /* Original getHeroes function
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  */
 }
