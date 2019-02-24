e6da217777aa4ce5ea729f1e98073e1a3ffa843d
TonyJulin <tonyjulin@gmail.com>
2019-02-23 21:52:33 -0700

    Bound message service to a template(html) to display the messages

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+
 import { Observable, of } from 'rxjs';
 
 @Injectable({
