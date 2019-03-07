8a812dbdb002396235f6e1c747f40cb8936cc0a3
TonyJulin <tonyjulin@gmail.com>
2019-03-06 21:45:30 -0700

    Heroes component class cleanup

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,15 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
-
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) {}
 
@@ -17,20 +7,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  // Functions
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  // getHeroes() with observable
   getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
-  /* Original getHeroes function
-  //Assigns array of heroes to components heroes property. Does so synchronously, would cause browser to freeze waiting for server.
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
-  */
 }
