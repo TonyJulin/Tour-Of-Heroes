b13fbcc3b2e76b7b12c25e5c30226a7cd2b9508c
TonyJulin <tonyjulin@gmail.com>
2019-03-06 21:44:51 -0700

    Dashboard and Heroes component pages go to Hero detail page when a hero is clicked

--- tour-of-heroes/src/app/app-routing.module.ts
+++ tour-of-heroes/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 
 // routes to localhost:4200/heroes
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'heroes', component: HeroesComponent }
+  // Default route
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+  // Parameterized route: match path pattern to hero detail view
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "heroes", component: HeroesComponent }
 ];
 
 @NgModule({
