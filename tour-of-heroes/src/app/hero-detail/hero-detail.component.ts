cface346b8b33b8aa3d2ecd81d3473ca25dda877
TonyJulin <tonyjulin@gmail.com>
2019-03-06 21:59:00 -0700

    Created routable hero detail component. Extracted the id from the route parameter, added getHero() method to HeroService

--- tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit, Input } from "@angular/core";
+import { Hero } from "../hero";
+
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
+import { HeroService } from "../hero.service";
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  selector: "app-hero-detail",
+  templateUrl: "./hero-detail.component.html",
+  styleUrls: ["./hero-detail.component.css"]
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    // holds information about the route to this instance of the HeroDetailComponent (e.g. id)
+    private route: ActivatedRoute,
+    // gets hero data from the remote server and this component will use it to get the hero-to-display
+    private heroService: HeroService,
+    // Angular service for interacting with the browser. Can navigate back to previous view
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    // get static image of route info shortly after component creation
+    // get id of hero to fetch
+    // convert string to number (+)
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
 }
--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -1,23 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-import { MessageService } from './message.service';
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
+import { MessageService } from "./message.service";
 
-import { Observable, of } from 'rxjs';
+import { Observable, of } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HeroService {
-
   getHeroes(): Observable<Hero[]> {
     // Send message after fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
+    this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
   }
 
-  // Service-in-service: inject MessageService into HeroService which is injected 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message after fetching the hero
+    this.messageService.add("HeroService: fetched hero id=${id}");
+    return of(HEROES.find(hero => hero.id == id));
+  }
+
+  // Service-in-service: inject MessageService into HeroService which is injected
   // into HeroesComponent
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {}
 }
