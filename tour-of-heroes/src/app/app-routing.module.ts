3a14e8b4950332505181cac48e5b5d2f1d29a230
TonyJulin <tonyjulin@gmail.com>
2019-03-06 21:33:42 -0700

    Added link to dashboard

--- tour-of-heroes/src/app/app-routing.module.ts
+++ tour-of-heroes/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 // routes to localhost:4200/heroes
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
 @NgModule({
-  exports: [ RouterModule ],
+  exports: [RouterModule],
   // Configure router at app root level
-  imports: [ RouterModule.forRoot(routes)]
+  imports: [RouterModule.forRoot(routes)]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
